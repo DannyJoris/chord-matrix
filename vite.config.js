133d53f4b4bf4cdeaf36c83bc8515125c121f8de
Danny Joris <danny.joris@lullabot.com>
2024-12-16 23:11:26 -0400

    Update tugboat config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/chord-matrix/',
+  base: process.env.NODE_ENV === 'production' ? '/chord-matrix/' : '/',
   build: {
     outDir: 'docs'
   }

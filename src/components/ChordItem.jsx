ab2911983a91ee9a53daea7b4d185f89dbc46551
Danny Joris <danny.joris@lullabot.com>
2025-02-24 16:44:05 -0400

    No M notation for major chords

--- src/components/ChordItem.jsx
+++ src/components/ChordItem.jsx
@@ -33,7 +33,7 @@ export const ChordItem = ({
       <span className="badge badge-top-left rounded-pill" style={{ backgroundColor: 'hotpink' }}>
         {activeChords.indexOf(cell) + 1}
       </span>
-      <strong>{info.tonic}{info.type}</strong>
+      <strong>{info.tonic}{info.type !== 'M' && info.type}</strong>
       {info.roman && <span className="badge badge-top-right rounded-pill bg-info ms-2">{info.roman}</span>}
       <div className="mt-2">{info.notes}</div>
       {removeMode && (
